20bce94351cf384378a18cb794b41b37d1e2ed6a
sandra365 <postnsanna@gmail.com>
2022-10-13 16:22:44 +0300

    created details component

--- src/ShipmentDetails.js
+++ src/ShipmentDetails.js
@@ -1,3 +1,75 @@
+import './index.css';
+
+
+function ShipmentDetails({ orderNo, date, customer, trackingNo, status, consignee, allShipments, updateState }) {
+    
+    
+    // const form = (
+        
+    //     <form>
+    //         <label>
+    //             orderNo:
+    //             <input type='text' value={orderNo}/>
+    //         </label>
+    //         <label>
+    //             date:
+    //             <input type='date' value={date}/>
+    //         </label>
+    //         <label>
+    //             customer:
+    //             <input type='text' value={customer}/>
+    //         </label>
+    //         <label>
+    //             trackingNo:
+    //             <input type='text' value={trackingNo}/>
+    //         </label>
+    //         <label>
+    //             consignee:
+    //             <input type='text' value={consignee}/>
+    //         </label>
+    //         <label>
+    //             status:
+    //             <input type='text' value={status}/>
+    //         </label>
+
+    //     </form>
+    // );
+        
+    const form = (
+        <div id='input-form'>
+            Hi, I'm floatin over table 😊
+        </div>
+    );
+
+
+    return(
+        form
+    );
+}
+
+
+export default ShipmentDetails;
+
+
+
+
+
+
+
+
+
+//<form onSubmit={handleSubmit}>
+
+// const handleSubmit= (event) => {
+    //     event.preventDefault();
+    // }
+
+
+
+
+
+
+
 // function FormInput(props) {
 //     return(
 //         <div>
