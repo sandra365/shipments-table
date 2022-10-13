278876ea9da5fb3458846169bcb2ca489d248149
sandra365 <postnsanna@gmail.com>
2022-10-13 16:47:00 +0300

    added App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import {hardcodedData} from './data.js';
+import ShipmentsTable from './ShipmentsTable';
+import ShipmentDetails from './ShipmentDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [shipmentsList, setShipmentsList] = useState(hardcodedData);
+    console.log('Shipments from ShipmentsTable', shipmentsList);
+  //const apiURL = 'https://my.api.mockaroo.com/shipments.json?key=5e0b62d0';
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const data = await axios.get(apiURL);
+  //     setShipmentsList(data);
+  //   };
+  //   getData();
+  // }, []);
+
+  //const shipments = shipmentsList.data ? shipmentsList.data : hardcodedData;//is it necessary/how to rewrite/put it outside so it won't load every time component being redrawn
+
+    return(
+        <div id='shipments-table-container' className='shipments-table-container'>
+            <ShipmentDetails />
+            <ShipmentsTable 
+                shipmentsList={shipmentsList}
+                setShipmentsList={setShipmentsList}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/ShipmentsTable.js
+++ src/ShipmentsTable.js
@@ -1,24 +1,8 @@
 import './App.css';
+import './index.css';
 import Shipment from './Shipment';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import {hardcodedData} from './data.js';
 
-function ShipmentsTable() {
-  const [shipmentsList, setShipmentsList] = useState(hardcodedData);
-  console.log('Shipments from ShipmentsTable', shipmentsList);
-  //const apiURL = 'https://my.api.mockaroo.com/shipments.json?key=5e0b62d0';
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const data = await axios.get(apiURL);
-  //     setShipmentsList(data);
-  //   };
-  //   getData();
-  // }, []);
-
-  //const shipments = shipmentsList.data ? shipmentsList.data : hardcodedData;//is it necessary/how to rewrite
-
+function ShipmentsTable({ shipmentsList, setShipmentsList }) {
   const shipmentsTableHeader = (
     <thead>
       <tr>
@@ -45,21 +29,19 @@ function ShipmentsTable() {
           status={status}
           consignee={consignee}
           allShipments={shipmentsList}
-          deleteShipment={setShipmentsList}
+          setShipmentsList={setShipmentsList}
         />
       );
     })
   );
     
   return (
-    <div className="shipments-table-container">
-      <table className="shipments-table">
-        {shipmentsTableHeader}
-        <tbody>
-          {shipmentComponentsList}
-        </tbody>
-      </table>
-    </div>
+    <table className='shipments-table'>
+      {shipmentsTableHeader}
+      <tbody>
+        {shipmentComponentsList}
+      </tbody>
+    </table>
   );
 }
 
